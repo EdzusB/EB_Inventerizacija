702646f782400622f85a8138d035386b9955f23a
EdzusB <edzusbleikss1@gmail.com>
2023-12-05 11:39:22 +0200

    Datubāzes izvade

--- EB_inventerizacija/Form4.Designer.cs
+++ EB_inventerizacija/Form4.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.konekcija_button = new System.Windows.Forms.Button();
+            this.izejviela_dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.izejviela_dgv)).BeginInit();
             this.SuspendLayout();
+            // 
+            // konekcija_button
+            // 
+            this.konekcija_button.Location = new System.Drawing.Point(359, 359);
+            this.konekcija_button.Name = "konekcija_button";
+            this.konekcija_button.Size = new System.Drawing.Size(75, 23);
+            this.konekcija_button.TabIndex = 0;
+            this.konekcija_button.Text = "Konekcija";
+            this.konekcija_button.UseVisualStyleBackColor = true;
+            this.konekcija_button.Click += new System.EventHandler(this.konekcija_button_Click);
+            // 
+            // izejviela_dgv
+            // 
+            this.izejviela_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.izejviela_dgv.Location = new System.Drawing.Point(97, 47);
+            this.izejviela_dgv.Name = "izejviela_dgv";
+            this.izejviela_dgv.Size = new System.Drawing.Size(630, 278);
+            this.izejviela_dgv.TabIndex = 1;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.izejviela_dgv);
+            this.Controls.Add(this.konekcija_button);
             this.Name = "Form4";
             this.Text = "Izejvielu lietojums";
+            ((System.ComponentModel.ISupportInitialize)(this.izejviela_dgv)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button konekcija_button;
+        private System.Windows.Forms.DataGridView izejviela_dgv;
     }
 }
--- EB_inventerizacija/Form4.cs
+++ EB_inventerizacija/Form4.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace EB_inventerizacija
 {
@@ -16,5 +15,82 @@ namespace EB_inventerizacija
         {
             InitializeComponent();
         }
+
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=EB_inventerizacija.db; Version=3; New=True; Compress=True;");
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception
+                MessageBox.Show("Failed to connect to the database.");
+            }
+            return sqlite_conn;
+        }
+
+        private async void konekcija_button_Click(object sender, EventArgs e)
+        {
+            await FetchAndUpdatePrices(); // Fetch data from the API and update the database
+
+            // Load updated data from the database after modification
+            using (SQLiteConnection sqlite_conn = CreateConnection())
+            {
+                sqlite_conn.Open();
+                SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT * FROM Izejviela";
+
+                DataTable sTable = new DataTable();
+                SQLiteDataAdapter sqlda = new SQLiteDataAdapter(sqlite_cmd);
+                sqlda.Fill(sTable);
+                izejviela_dgv.DataSource = sTable;
+            }
+        }
+
+        static async Task FetchAndUpdatePrices()
+        {
+            string apiUrl = "https://fakestoreapi.com/products";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    var products = JsonConvert.DeserializeObject<List<Cena>>(jsonData);
+
+                    foreach (var product in products)
+                    {
+                        InsertProductIntoDatabase(product.cena);
+                    }
+
+                    MessageBox.Show("Prices updated in the database.");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to fetch data from the API.");
+                }
+            }
+        }
+
+        static void InsertProductIntoDatabase(double price)
+        {
+            using (var connection = CreateConnection())
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO Izejviela (cena) VALUES (@Price)";
+                cmd.Parameters.AddWithValue("@Price", price);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    class Cena
+    {
+        public double cena { get; set; }
     }
 }
