7354ca3cd051a33652b4274864919c3314db774e
EdzusB <edzusbleikss1@gmail.com>
2023-12-12 11:36:52 +0200

    Salabota datu izgūšana no datubāzes

--- EB_inventerizacija/Form4.Designer.cs
+++ EB_inventerizacija/Form4.Designer.cs
@@ -30,14 +30,19 @@
         {
             this.konekcija_button = new System.Windows.Forms.Button();
             this.izejviela_dgv = new System.Windows.Forms.DataGridView();
+            this.inventars_cbox = new System.Windows.Forms.ComboBox();
+            this.skaits = new System.Windows.Forms.TextBox();
+            this.prece_dgv = new System.Windows.Forms.DataGridView();
+            this.saglabat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.izejviela_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prece_dgv)).BeginInit();
             this.SuspendLayout();
             // 
             // konekcija_button
             // 
-            this.konekcija_button.Location = new System.Drawing.Point(359, 359);
+            this.konekcija_button.Location = new System.Drawing.Point(209, 313);
             this.konekcija_button.Name = "konekcija_button";
-            this.konekcija_button.Size = new System.Drawing.Size(75, 23);
+            this.konekcija_button.Size = new System.Drawing.Size(100, 33);
             this.konekcija_button.TabIndex = 0;
             this.konekcija_button.Text = "Konekcija";
             this.konekcija_button.UseVisualStyleBackColor = true;
@@ -46,22 +51,69 @@
             // izejviela_dgv
             // 
             this.izejviela_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.izejviela_dgv.Location = new System.Drawing.Point(97, 47);
+            this.izejviela_dgv.Location = new System.Drawing.Point(12, 12);
             this.izejviela_dgv.Name = "izejviela_dgv";
-            this.izejviela_dgv.Size = new System.Drawing.Size(630, 278);
+            this.izejviela_dgv.Size = new System.Drawing.Size(470, 295);
             this.izejviela_dgv.TabIndex = 1;
+            // 
+            // inventars_cbox
+            // 
+            this.inventars_cbox.FormattingEnabled = true;
+            this.inventars_cbox.Items.AddRange(new object[] {
+            "Vīriešu T-krekls",
+            "Vīriešu jaka",
+            "Vīriešu džemperis",
+            "Sieviešu jaka",
+            "Sieviešu lietusmētelis",
+            "Sieviešu T-krekls"});
+            this.inventars_cbox.Location = new System.Drawing.Point(598, 12);
+            this.inventars_cbox.Name = "inventars_cbox";
+            this.inventars_cbox.Size = new System.Drawing.Size(121, 21);
+            this.inventars_cbox.TabIndex = 2;
+            this.inventars_cbox.SelectedIndexChanged += new System.EventHandler(this.inventars_cbox_SelectedIndexChanged);
+            // 
+            // skaits
+            // 
+            this.skaits.Location = new System.Drawing.Point(598, 195);
+            this.skaits.Name = "skaits";
+            this.skaits.Size = new System.Drawing.Size(100, 20);
+            this.skaits.TabIndex = 3;
+            // 
+            // prece_dgv
+            // 
+            this.prece_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.prece_dgv.Location = new System.Drawing.Point(532, 39);
+            this.prece_dgv.Name = "prece_dgv";
+            this.prece_dgv.Size = new System.Drawing.Size(240, 150);
+            this.prece_dgv.TabIndex = 4;
+            // 
+            // saglabat
+            // 
+            this.saglabat.Location = new System.Drawing.Point(608, 221);
+            this.saglabat.Name = "saglabat";
+            this.saglabat.Size = new System.Drawing.Size(75, 23);
+            this.saglabat.TabIndex = 5;
+            this.saglabat.Text = "Saglabāt";
+            this.saglabat.UseVisualStyleBackColor = true;
+            this.saglabat.Click += new System.EventHandler(this.saglabat_Click);
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saglabat);
+            this.Controls.Add(this.prece_dgv);
+            this.Controls.Add(this.skaits);
+            this.Controls.Add(this.inventars_cbox);
             this.Controls.Add(this.izejviela_dgv);
             this.Controls.Add(this.konekcija_button);
             this.Name = "Form4";
             this.Text = "Izejvielu lietojums";
             ((System.ComponentModel.ISupportInitialize)(this.izejviela_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prece_dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +121,9 @@
 
         private System.Windows.Forms.Button konekcija_button;
         private System.Windows.Forms.DataGridView izejviela_dgv;
+        private System.Windows.Forms.ComboBox inventars_cbox;
+        private System.Windows.Forms.TextBox skaits;
+        private System.Windows.Forms.DataGridView prece_dgv;
+        private System.Windows.Forms.Button saglabat;
     }
 }
--- EB_inventerizacija/Form4.cs
+++ EB_inventerizacija/Form4.cs
@@ -5,7 +5,6 @@ using System.Data.SQLite;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace EB_inventerizacija
 {
@@ -19,78 +18,129 @@ namespace EB_inventerizacija
         static SQLiteConnection CreateConnection()
         {
             SQLiteConnection sqlite_conn;
-            sqlite_conn = new SQLiteConnection("Data Source=EB_inventerizacija.db; Version=3; New=True; Compress=True;");
+            sqlite_conn = new SQLiteConnection("Data Source=EB_inventerizacija.db; Version=3; New=True; Compress=True; ");
             try
             {
                 sqlite_conn.Open();
             }
             catch (Exception ex)
             {
-                // Handle the exception
-                MessageBox.Show("Failed to connect to the database.");
+                MessageBox.Show("Konekcija neizdevās!");
             }
             return sqlite_conn;
         }
 
         private async void konekcija_button_Click(object sender, EventArgs e)
         {
-            await FetchAndUpdatePrices(); // Fetch data from the API and update the database
+            SQLiteConnection sqlite_conn = CreateConnection();
 
-            // Load updated data from the database after modification
-            using (SQLiteConnection sqlite_conn = CreateConnection())
+            SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * From Izejviela";
+
+            DataTable sTable = new DataTable();
+            SQLiteDataAdapter sqlda = new SQLiteDataAdapter(sqlite_cmd);
+            sqlda.Fill(sTable);
+            izejviela_dgv.DataSource = sTable;
+        }
+
+        private void inventars_cbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (inventars_cbox.SelectedItem != null)
             {
-                sqlite_conn.Open();
-                SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
-                sqlite_cmd.CommandText = "SELECT * FROM Izejviela";
+                string selectedValue = inventars_cbox.SelectedItem.ToString();
 
-                DataTable sTable = new DataTable();
+                CreateConnection();
+                SQLiteConnection sqlite_conn;
+                sqlite_conn = CreateConnection();
+
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT * FROM Izejviela WHERE Nosaukums = @SelectedValue";
+                sqlite_cmd.Parameters.AddWithValue("@SelectedValue", selectedValue);
+
+                DataTable sTable;
                 SQLiteDataAdapter sqlda = new SQLiteDataAdapter(sqlite_cmd);
-                sqlda.Fill(sTable);
-                izejviela_dgv.DataSource = sTable;
-            }
-        }
-
-        static async Task FetchAndUpdatePrices()
-        {
-            string apiUrl = "https://fakestoreapi.com/products";
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-                if (response.IsSuccessStatusCode)
+                using (sTable = new DataTable())
                 {
-                    string jsonData = await response.Content.ReadAsStringAsync();
-                    var products = JsonConvert.DeserializeObject<List<Cena>>(jsonData);
-
-                    foreach (var product in products)
-                    {
-                        InsertProductIntoDatabase(product.cena);
-                    }
-
-                    MessageBox.Show("Prices updated in the database.");
-                }
-                else
-                {
-                    MessageBox.Show("Failed to fetch data from the API.");
+                    sqlda.Fill(sTable);
+                    prece_dgv.DataSource = sTable;
                 }
             }
+            
         }
 
-        static void InsertProductIntoDatabase(double price)
+        private void saglabat_Click(object sender, EventArgs e)
         {
-            using (var connection = CreateConnection())
+            int minDaudzums = 0;
+            int maxDaudzums = 0;
+            int pieejamaisDaudzums = 0;
+            decimal cena = 0;
+            int Skaits = 0;
+            int atlikums = 0;
+            decimal izmaksas = 0;
+            int vajadzigs = 0;
+
+            if (int.TryParse(skaits.Text, out int parsedSkaits))
             {
-                connection.Open();
-                var cmd = connection.CreateCommand();
-                cmd.CommandText = "INSERT INTO Izejviela (cena) VALUES (@Price)";
-                cmd.Parameters.AddWithValue("@Price", price);
-                cmd.ExecuteNonQuery();
+                Skaits = parsedSkaits;
+            }
+            else
+            {
+                Skaits = 0;
+            }
+
+            if (inventars_cbox.SelectedItem != null)
+            {
+                string selectedValue = inventars_cbox.SelectedItem.ToString();
+
+                CreateConnection();
+                SQLiteConnection sqlite_conn;
+                sqlite_conn = CreateConnection();
+
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT * FROM Izejviela WHERE Nosaukums = @SelectedValue";
+                sqlite_cmd.Parameters.AddWithValue("@SelectedValue", selectedValue);
+
+                DataTable sTable;
+                SQLiteDataAdapter sqlda = new SQLiteDataAdapter(sqlite_cmd);
+                using (sTable = new DataTable())
+                {
+                    sqlda.Fill(sTable);
+                    prece_dgv.DataSource = sTable;
+                }
+
+                if (sTable.Rows.Count > 0)
+                {
+                    minDaudzums = Convert.ToInt32(sTable.Rows[0]["min_daudzums"]);
+                    maxDaudzums = Convert.ToInt32(sTable.Rows[0]["max_daudzums"]);
+                    pieejamaisDaudzums = Convert.ToInt32(sTable.Rows[0]["pieejamais_daudzums"]);
+                    cena = Convert.ToDecimal(sTable.Rows[0]["cena"]);
+
+                }
+
+                if (pieejamaisDaudzums - Skaits < 0)
+                {
+                    MessageBox.Show("Krājumos trūkst preču!");
+                }
+
+                atlikums = pieejamaisDaudzums - Skaits;
+
+                if(atlikums <= minDaudzums)
+                {
+                    vajadzigs = maxDaudzums - atlikums;
+                    izmaksas = vajadzigs * cena;
+
+                    sqlite_cmd.CommandText = "UPDATE Izejviela SET pieejamais_daudzums = @NewPieejamais WHERE Nosaukums = @SelectedValue";
+                    sqlite_cmd.Parameters.AddWithValue("@NewPieejamais", maxDaudzums);
+                    sqlite_cmd.ExecuteNonQuery();
+                }
+
+
             }
         }
-    }
-
-    class Cena
-    {
-        public double cena { get; set; }
     }
 }
+
+
+
